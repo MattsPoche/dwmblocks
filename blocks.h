8c49a30e25960438cf19797e640ef85f5cf54e40
Matt <matthewspoche@comcast.net>
2020-06-05 22:27:02 -0500

    Initial commit for fork

--- blocks.h
+++ blocks.h
@@ -1,19 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 	
+	{"🎵", "mpc | sed q", 20, 0},
+	
+	{"💾", "df -h | awk '/sda4/ { print \"/home: \"$3\"/\"$2\" \"$5 }'", 60, 0 },
+
 	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
 
 	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
 
-	{"", "~/bin/statusbar/clock",						5,		0},
+	{"🕑", "date '+%F (%a) %R'",          60, 						0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
